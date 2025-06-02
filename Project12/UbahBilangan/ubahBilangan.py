06bf551965dd42ea00ac84dc4786172369ce4e0b
DedesuryanaTI24F <suryanadede1216@gmail.com>
2025-06-03 03:59:12 +0700

    Takbir

---
+++ Project12/UbahBilangan/ubahBilangan.py
@@ -0,0 +1,9 @@
+def decimalToBinary(decimal_number):
+    return bin(decimal)
+
+def decimalToOctal(decimal_number):
+    return oct(decimal)
+
+def decimalToHexa(decimal_number):
+    return hex(decimal)
+
